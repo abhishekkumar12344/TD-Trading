01fac89e8658848ed22ddf4ebd0820ecd3a70a6b
Abhishek Raj <abhishekraj90049@gmail.com>
2025-12-14 04:19:33 +0530

    add all

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,28 @@
-// import React from "react";
+// // import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import { BrowserRouter } from "react-router-dom";
+// import { AuthProvider } from "./context/AuthContext";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <AuthProvider>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </AuthProvider>
+// );
+
+
+
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </AuthProvider>
 );
